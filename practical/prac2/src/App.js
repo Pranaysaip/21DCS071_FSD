af8ef3a9fee82c0fb9024173a081bb6fd8c390b3
pranaysai <pranaysaipasumarthi@gmail.com>
2023-07-12 09:54:48 +0530

    first commit

---
+++ practical/prac2/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+
+
+function App() {
+ return(
+ <div> 
+ <BrowserRouter>
+ <Routes>
+  <Route exact path="/" element={<Home/>}/>
+  <Route exact path="/About" element={<About/>}/>
+  <Route exact path="/Contact" element={<Contact/>}/>
+ </Routes>
+ </BrowserRouter>
+ </div>
+ )
+ }
+
+export default App;
